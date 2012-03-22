450a9e9962541ffb96d8fce0e2080d4ac26e7697
Ethan Kaminski <ethan.kaminski@gmail.com>
2012-03-23 09:49:26 +1100

    Fix ANOTHER rounding bug
    Right, negative numbers cast to ints differently. Fixing that one, as well. I think that's all for now?

--- source/GridHelper.cpp
+++ source/GridHelper.cpp
@@ -8,9 +8,13 @@ Ogre::Vector3 GridHelper::roundToGrid(const Ogre::Vector3 vec)
 Ogre::Real GridHelper::roundToGrid(const Ogre::Real num)
 {
 	int roundedNum = static_cast<int>(num / GRID_SIZE) * GRID_SIZE;
-	if (num - roundedNum >= GRID_SIZE / 2)
+	if (num >= 0 && num - roundedNum >= GRID_SIZE / 2)
 	{
 		roundedNum += GRID_SIZE;
+	}
+	else if (num <= 0 && roundedNum - num >= GRID_SIZE / 2)
+	{
+		roundedNum -= GRID_SIZE;
 	}
 	return roundedNum;
 }
